77d4cb21e015a1949b6c6d668f13801db693c541
wuyinjun <yinjun.wu@yintech.cn>
2019-09-09 18:26:58 +0800

    新增一种订阅解析方法

--- V2RayC/Src/Models/ProxyModel.swift
+++ V2RayC/Src/Models/ProxyModel.swift
@@ -64,7 +64,7 @@ class ProxyModel: NSObject, Mappable {
     var path: String?
     // 是否被选中
     var isSelected: Bool = false
-    
+
     // MARK: - Public Methods
     func saveToDisk() {
         if let jsonString = toJSONString() {
@@ -72,17 +72,17 @@ class ProxyModel: NSObject, Mappable {
             try? (jsonString as NSString).write(toFile: path, atomically: false, encoding: String.Encoding.utf8.rawValue)
         }
     }
-    
+
     func deleteFromDisk() {
         let path = "\(kV2rayConfigFolderPath)\(remarks ?? "default").json"
         try? FileManager.default.removeItem(atPath: path)
     }
-    
+
     // MARK: - Mappable
     required init?(map: Map) { }
     override init() {
     }
-    
+
     func mapping(map: Map) {
         from <- map["from"]
         address <- map["address"]
@@ -97,71 +97,93 @@ class ProxyModel: NSObject, Mappable {
         host <- map["host"]
         path <- map["path"]
     }
-    
+
     // MARK: - Life Cycle
-    convenience init(dict: [String: String]) {
+    convenience init(dict: [String: AnyObject]) {
         self.init()
         if let ps = dict["ps"] {
-            remarks = ps
+            if let ps = ps as? String {
+                remarks = ps
+            }
         }
         if let add = dict["add"] {
-            address = add
+            if let add = add as? String {
+                address = add
+            }
         }
         if let p = dict["port"] {
-            port = Int(p)
+            if let p = p as? String {
+                port = Int(p)
+            } else if let p = p as? Int64 {
+                port = Int(p)
+            }
         }
         if let idd = dict["id"] {
-            id = idd
+            if let idd = idd as? String {
+                id = idd
+            }
         }
         if let aid = dict["aid"] {
-            alterID = Int(aid)
+            if let aid = aid as? String {
+                alterID = Int(aid)
+            }
         }
         if let net = dict["net"] {
-            switch net {
-            case "tcp":
-                network = ProxyNetwork.tcp
-                break
-            case "kcp":
-                network = ProxyNetwork.kcp
-                break
-            case "ws":
-                network = ProxyNetwork.ws
-                break
-            case "h2":
-                network = ProxyNetwork.http2
-                break
-            default:
-                network = ProxyNetwork.tcp
-                break
+            if let net = net as? String {
+                switch net {
+                case "tcp":
+                    network = ProxyNetwork.tcp
+                    break
+                case "kcp":
+                    network = ProxyNetwork.kcp
+                    break
+                case "ws":
+                    network = ProxyNetwork.ws
+                    break
+                case "h2":
+                    network = ProxyNetwork.http2
+                    break
+                default:
+                    network = ProxyNetwork.tcp
+                    break
+                }
             }
         }
         if let t = dict["tls"] {
-            tls = t == "tls"
+            if let t = t as? String {
+                tls = t == "tls"
+            }
         }
         if let sec = dict["type"] {
-            switch sec {
-            case "chacha20-poly1305":
-                security = ProxySecurity.chacha20poly1305
-                break
-            case "aes-128-cfb":
-                security = ProxySecurity.aes128cfb
-                break
-            case "aes-128-gcm":
-                security = ProxySecurity.aes128gcm
-                break
-            case "none":
-                security = ProxySecurity.none
-                break
-            default:
-                security = ProxySecurity.none
-                break
+            if let sec = sec as? String {
+                switch sec {
+                case "chacha20-poly1305":
+                    security = ProxySecurity.chacha20poly1305
+                    break
+                case "aes-128-cfb":
+                    security = ProxySecurity.aes128cfb
+                    break
+                case "aes-128-gcm":
+                    security = ProxySecurity.aes128gcm
+                    break
+                case "none":
+                    security = ProxySecurity.none
+                    break
+                default:
+                    security = ProxySecurity.none
+                    break
+                }
             }
         }
         if let h = dict["host"] {
-            host = h
+            if let h = h as? String {
+                host = h
+            }
         }
         if let p = dict["path"] {
-            path = p
+            if let p = p as? String {
+                path = p
+            }
         }
     }
 }
--- V2RayC/Src/Utils/VmessUtils.swift
+++ V2RayC/Src/Utils/VmessUtils.swift
@@ -12,11 +12,11 @@ func genProxyModelFrom(vmessURL: String) -> ProxyModel? {
     guard vmessURL.hasPrefix("vmess://") else {
         return nil
     }
-    
-    let vmessBase64String = String(vmessURL[vmessURL.index(vmessURL.startIndex, offsetBy: 8)...])
-    if let date  = Data(base64Encoded: vmessBase64String), let vmessJsonString = String(data: date, encoding: String.Encoding.utf8) {
-        
-        if let dict = convertStringToDictionary(text: vmessJsonString) as? [String: String] {
+
+    var vmessBase64String = String(vmessURL[vmessURL.index(vmessURL.startIndex, offsetBy: 8)...])
+    if let date = Data(base64Encoded: vmessBase64String), let vmessJsonString = String(data: date, encoding: String.Encoding.utf8) {
+
+        if let dict = convertStringToDictionary(text: vmessJsonString) {
             let model = ProxyModel(dict: dict)
             model.from = ProxyFrom.subscribtion
             return model
@@ -27,18 +27,23 @@ func genProxyModelFrom(vmessURL: String) -> ProxyModel? {
 
 func fetchProxyModelsFrom(url: String) {
     _ = fetchSubscribeContentFrom(url: url).drive(onNext: { (content) in
-        handleSubscribeInfoType1(contentString: content)
+        if handleSubscribeInfoType1(contentString: content) {
+            
+        }
+        if handleSubscribeInfoType2(contentString: content) {
+            
+        }
     }, onCompleted: {
-        NotificationCenter.default.post(Notification.init(name: Notification.Name(rawValue: kSubscribeRefreshComplete)))
-    }, onDisposed: {
+            NotificationCenter.default.post(Notification.init(name: Notification.Name(rawValue: kSubscribeRefreshComplete)))
+        }, onDisposed: {
 
-    })
+        })
 }
 
 /// 处理订阅来的Proxy, Type1: 先base64解码，里面是vmess://开头的链接
 ///
 /// - Parameter contentString: 从订阅网址得到的字符串
-private func handleSubscribeInfoType1(contentString: String) {
+private func handleSubscribeInfoType1(contentString: String) -> Bool {
     var proxyModels = [ProxyModel]()
     if let decodeData = Data(base64Encoded: contentString), let decodeString = String(data: decodeData, encoding: String.Encoding.utf8) {
         let vmessList = decodeString.components(separatedBy: "\r\n")
@@ -48,6 +53,31 @@ private func handleSubscribeInfoType1(contentString: String) {
             }
         }
     }
-    ProxyListManager.shared.addProxy(list: proxyModels, at: nil)
+    if proxyModels.count > 0 {
+        ProxyListManager.shared.addProxy(list: proxyModels, at: nil)
+        return true
+    } else {
+        return false
+    }
 }
 
+/// 处理订阅来的Proxy, Type1: 先base64解码，里面是vmess://开头的链接
+///
+/// - Parameter contentString: 从订阅网址得到的字符串
+private func handleSubscribeInfoType2(contentString: String) -> Bool {
+    var proxyModels = [ProxyModel]()
+    if let decodeData = Data(base64Encoded: contentString), let decodeString = String(data: decodeData, encoding: String.Encoding.utf8) {
+        let vmessList = decodeString.components(separatedBy: "\n")
+        for vmessUrl in vmessList {
+            if let model = genProxyModelFrom(vmessURL: vmessUrl) {
+                proxyModels.append(model)
+            }
+        }
+    }
+    if proxyModels.count > 0 {
+        ProxyListManager.shared.addProxy(list: proxyModels, at: nil)
+        return true
+    } else {
+        return false
+    }
+}
